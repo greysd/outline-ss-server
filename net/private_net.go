66de098e791ec2b210a0070384ca1178e3ebb112
greysd <37036466+greysd@users.noreply.github.com>
2024-03-29 23:10:56 +0300

    Update private_net.go
    Remove private ips

--- net/private_net.go
+++ net/private_net.go
@@ -24,9 +24,9 @@ var privateNetworks []*net.IPNet
 func init() {
 	for _, cidr := range []string{
 		// RFC 1918: private IPv4 networks
-		"10.0.0.0/8",
-		"172.16.0.0/12",
-		"192.168.0.0/16",
+		// "10.0.0.0/8",
+		// "172.16.0.0/12",
+		// "192.168.0.0/16",
 		// RFC 4193: IPv6 ULAs
 		"fc00::/7",
 		// RFC 6598: reserved prefix for CGNAT
